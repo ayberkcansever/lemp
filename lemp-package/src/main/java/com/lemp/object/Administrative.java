4461c96c9488639e729832887ae4133d90d73a79
Ayberk Cansever <ayberkcansever@Ayberks-MacBook-Pro.local>
2017-07-13 11:14:31 +0300

    administrative requests added

---
+++ lemp-package/src/main/java/com/lemp/object/Administrative.java
@@ -0,0 +1,7 @@
+package com.lemp.object;
+
+/**
+ * Created by ayberkcansever on 13/07/2017.
+ */
+public class Administrative {
+}
---
+++ lemp-server/src/test/java/com/lemp/server/akka/actor/AdministrativeRequestProcessorActorTest.java
@@ -0,0 +1,37 @@
+package com.lemp.server.akka.actor;
+
+import akka.testkit.TestKit;
+import com.lemp.server.BaseTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import scala.concurrent.duration.Duration;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by AyberkC on 09.06.2017.
+ */
+public class AdministrativeRequestProcessorActorTest extends BaseTest {
+
+    @BeforeClass
+    public static void setup() {
+        setUp();
+    }
+
+    @Test
+    public void testAuthentication() throws InterruptedException, IOException {
+        new TestKit(system) {{
+            authenticateUser(session, testUsername, testUserPassword);
+            TimeUnit.SECONDS.sleep(1);
+            logoutUser(session);
+        }};
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system, Duration.create(10, TimeUnit.SECONDS), true);
+        system = null;
+    }
+}
