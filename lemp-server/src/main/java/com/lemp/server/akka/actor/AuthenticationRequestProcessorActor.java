79ea05170eb5d95bc7c6b0a42ed44def6e35308f
Ayberk Cansever <ayberkcansever@Ayberks-MacBook-Pro.local>
2017-07-15 21:15:42 +0300

    existing actor handling

--- lemp-server/src/main/java/com/lemp/server/akka/actor/AuthenticationRequestProcessorActor.java
+++ lemp-server/src/main/java/com/lemp/server/akka/actor/AuthenticationRequestProcessorActor.java
@@ -1,17 +1,20 @@
 package com.lemp.server.akka.actor;
 
-import akka.actor.Props;
-import akka.actor.UntypedActor;
+import akka.actor.*;
+import akka.cluster.pubsub.DistributedPubSub;
+import akka.cluster.pubsub.DistributedPubSubMediator;
 import com.google.gson.Gson;
 import com.lemp.object.Authentication;
 import com.lemp.packet.Datum;
 import com.lemp.packet.Request;
 import com.lemp.packet.Response;
+import com.lemp.server.Application;
 import com.lemp.server.akka.object.SessionRequest;
 import com.lemp.server.database.UserDBHelper;
 import com.lemp.server.database.dbo.User;
 
 import javax.websocket.Session;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ayberkcansever on 15/01/17.
@@ -36,8 +39,14 @@ public class AuthenticationRequestProcessorActor extends UntypedActor {
                 User user = UserDBHelper.getInstance().isAuthenticatedUser(identity, token);
                 response.setResult(user != null ? 1 : 0);
                 if(response.getResult() == 1) {
+                    try {
+                        getContext().system().actorOf(Props.create(SessionActor.class, session), identity);
+                    } catch (InvalidActorNameException ex) {
+                        DistributedPubSub.get(Application.getActorSystem()).mediator().tell(new DistributedPubSubMediator.Send("/user/" + identity, PoisonPill.getInstance(), false), ActorRef.noSender());
+                        TimeUnit.SECONDS.sleep(2);
+                        getContext().system().actorOf(Props.create(SessionActor.class, session), identity);
+                    }
                     session.getUserProperties().put(ActorProperties.USER, user);
-                    getContext().system().actorOf(Props.create(SessionActor.class, session), identity);
                 }
                 session.getBasicRemote().sendText(gson.toJson(new Datum(response)));
             } catch (Exception e) {
